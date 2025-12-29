efb4550a503befcdf2b011f7f0580b327eb79a2d
ashrofromansyah-sketch <ashrofromansyah@gmail.com>
2025-12-29 19:30:03 +0700

    Create Mahasiswa.h

---
+++ Mahasiswa.h
@@ -0,0 +1,10 @@
+#ifndef MAHASISWA_H
+#define MAHASISWA_H
+
+#include <vector>
+using namespace std;
+
+int totalNilaiIteratif(const vector<int>& nilai);
+int totalNilaiRekursif(const vector<int>& nilai, int n);
+
+#endif
